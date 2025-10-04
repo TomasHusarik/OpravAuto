77cd4a4a069754428d11e6c35d4fd97a0983c81e
Tomáš Husarik <malejhusa@gmail.com>
2025-10-04 21:34:39 +0200

    Frontend login and firs components

---
+++ client/src/components/mantine/NavbarSimple.tsx
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+import {
+  Icon2fa,
+  IconBellRinging,
+  IconDatabaseImport,
+  IconFingerprint,
+  IconKey,
+  IconLogout,
+  IconReceipt2,
+  IconSettings,
+  IconSwitchHorizontal,
+} from '@tabler/icons-react';
+import { Group, Image, Text } from '@mantine/core';
+import classes from './NavbarSimple.module.css';
+import { ActionToggle } from './ActionToggle';
+
+const data = [
+  { link: '', label: 'Notifications', icon: IconBellRinging },
+  { link: '', label: 'Billing', icon: IconReceipt2 },
+  { link: '', label: 'Security', icon: IconFingerprint },
+  { link: '', label: 'SSH Keys', icon: IconKey },
+  { link: '', label: 'Databases', icon: IconDatabaseImport },
+  { link: '', label: 'Authentication', icon: Icon2fa },
+  { link: '', label: 'Other Settings', icon: IconSettings },
+];
+
+export function NavbarSimple() {
+  const [active, setActive] = useState('Billing');
+
+  const links = data.map((item) => (
+    <a
+      className={classes.link}
+      data-active={item.label === active || undefined}
+      href={item.link}
+      key={item.label}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(item.label);
+      }}
+    >
+      <item.icon className={classes.linkIcon} stroke={1.5} />
+      <span>{item.label}</span>
+    </a>
+  ));
+
+  return (
+    <nav className={classes.navbar}>
+      <div className={classes.navbarMain}>
+        <Group className={classes.header} justify="space-between">
+          <Image src="/opravAuto.png" alt="OpravAuto Logo" w={32} h={32} style={{marginLeft: "10px"}} />
+            <Text 
+              fw={700} 
+              size="lg" 
+              c="blue.6"
+              style={{ 
+                fontFamily: 'system-ui, -apple-system, sans-serif',
+                letterSpacing: '0.5px',
+                marginLeft: '-60px',
+              }}
+            >
+              OpravAuto
+            </Text>
+            <ActionToggle />
+          {/* <Code fw={700}>v1.0.0</Code> */}
+        </Group>
+        {links}
+      </div>
+
+      <div className={classes.footer}>
+        <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
+          <IconSwitchHorizontal className={classes.linkIcon} stroke={1.5} />
+          <span>Change account</span>
+        </a>
+
+        <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
+          <IconLogout className={classes.linkIcon} stroke={1.5} />
+          <span>Logout</span>
+        </a>
+      </div>
+    </nav>
+  );
+}
